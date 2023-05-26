b13c940ad48c2c52135d9facefa991551d51e4a7
Mauricio Bustos <mauricio.n.bustos@gmail.com>
2023-05-26 18:09:25 -0300

    Ejercicio 3 terminado

---
+++ src/Coche.java
@@ -0,0 +1,11 @@
+
+public class Coche {
+	
+	String color = "rojo";
+	int puertas = 4;
+	
+	public void agregarPuertas() {
+		this.puertas++;
+	}
+	
+}
--- src/ExercisesTema3.java
+++ src/ExercisesTema3.java
@@ -1,1 +1,20 @@
 
+public class ExercisesTema3 {
+	
+	public static void main(String[] args) {
+		
+		int resultado = suma(11, 34, 45);
+		System.out.println(resultado);
+		
+		Coche miCoche = new Coche();
+		System.out.println("Mi coche rojo tiene " + miCoche.puertas + " puertas");
+		miCoche.agregarPuertas();
+		System.out.println("Mi coche rojo ahora tiene " + miCoche.puertas + " puertas");
+	}
+	
+	public static int suma(int x, int y, int z) {
+		return x + y + z;
+	}
+	
+	
+}
